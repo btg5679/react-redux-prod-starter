f60999b19fbefc3300c9b9b1c2124496fb7ce4ce
Ben Garrison <ben.garrison@bens-mbp.lan>
2018-07-04 21:56:35 -0400

    eslint/airbnb

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,76 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import "../styles/App.css";
-import logo from "../logo.svg";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../styles/App.css';
+import Helmet from 'react-helmet';
+import { StyleSheet, css } from 'aphrodite';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {};
 };
 
+const styles = StyleSheet.create({
+  root: {
+    maxWidth: 700,
+    color: '#000',
+    margin: '2rem auto',
+  },
+  title: {
+    color: '#000',
+    maxWidth: 300,
+    fontSize: 56,
+  },
+  footer: {
+    margin: '4rem auto',
+    textAlign: 'center',
+    color: '#b7b7b7',
+  },
+  footerLink: {
+    display: 'inline-block',
+    color: '#000',
+    textDecoration: 'none',
+  },
+});
+
 class App extends Component {
+  updateState() {
+
+  }
+
   render() {
     return (
-      <div className="app">
-        <header className="appHeader">
-          <img src={logo} className="appLogo" alt="logo" />
-          <h1 className="appTitle">Welcome to React</h1>
-        </header>
-        <p className="appIntro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={css(styles.root)}>
+        <Helmet
+          title="React Production Starter"
+          titleTemplate="%s - React Production Starter"
+        />
+        <h1 className={css(styles.title)}>
+          React
+          Redux
+          Starter
+        </h1>
+        <footer className={css(styles.footer)}>
+        Twitter:
+          {' '}
+          <a
+            className={css(styles.footerLink)}
+            href="http://twitter.com/_bengarrison"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          @_bengarrison
+          </a>
+          <br />
+          Github:
+          {' '}
+          <a
+            className={css(styles.footerLink)}
+            href="https://github.com/btg5679/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          @btg5679
+          </a>
+        </footer>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import App from "./containers/App";
-import "./index.css";
-import reducers from "./reducers";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import App from './containers/App';
+import './index.css';
+import reducers from './reducers';
+import registerServiceWorker from './registerServiceWorker';
 
 let store;
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   store = createStore(combineReducers({ reducers }), composeWithDevTools(applyMiddleware(thunk)));
 } else {
   store = createStore(combineReducers({ reducers }), applyMiddleware(thunk));
@@ -20,6 +20,6 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 registerServiceWorker();

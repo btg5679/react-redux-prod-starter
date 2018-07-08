738a5f87899a54968a0b4a2ea40c3340e1c79f0b
Ben Garrison <ben.garrison@bens-mbp.lan>
2018-07-08 12:16:46 -0400

    update unit test example to handle router/aphrodite

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from 'redux-mock-store';
+import { StyleSheetTestUtils } from 'aphrodite';
 import App from '../containers/App';
 
+const initialState = { output: 100 };
+const mockStore = configureStore();
+let store;
+let container;
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+  store = mockStore(initialState);
+});
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
 it('renders without crashing', () => {
+  store = mockStore(initialState);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(<App store={store} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });

65ebd801caf83571c0c43ca976f95de27bdb1cca
Ben Garrison <ben.garrison@bens-mbp.lan>
2018-07-08 11:15:23 -0400

    webpack config/dependencies for prod/dev

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    'react-hot-loader/patch',
+    path.join(__dirname, './src/index.js'),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.json?$/,
+        use: 'json',
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'file-loader' },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.join(__dirname, 'node_modules')],
+  },
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StatsPlugin = require('stats-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  // The entry file. All your app roots from here.
+  entry: [
+    // Polyfills go here too, like babel-polyfill or whatwg-fetch
+    'babel-polyfill',
+    path.join(__dirname, './src/index.js'),
+  ],
+  // Where you want the output to go
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main-[hash].min.js',
+  },
+  plugins: [
+    // webpack gives your modules and chunks ids to identify them. Webpack can vary the
+    // distribution of the ids to get the smallest id length for often used ids with
+    // this plugin
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
+    // handles creating an index.html file and injecting assets. necessary because assets
+    // change name because the hash part changes. We want hash name changes to bust cache
+    // on client browsers.
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+    // extracts the css from the js files and puts them on a separate .css file. this is for
+    // performance and is used in prod environments. Styles load faster on their own .css
+    // file as they dont have to wait for the JS to load.
+    new ExtractTextPlugin('main-[hash].min.css'),
+    // creates a stats.json
+    new StatsPlugin('webpack.stats.json', {
+      source: false,
+      modules: false,
+    }),
+    // plugin for passing in data to the js, like what NODE_ENV we are in.
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
+  module: {
+    // loaders handle the assets, like transforming sass to css or jsx to js.
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.json?$/,
+        use: 'json',
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'file-loader' },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
